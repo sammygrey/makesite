d3b63926320359ca8e6bc6d14e0738eaa24dd893
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2022-02-16 01:43:58 -0500

    v1.0 part 2 done
    check the readme for specifics, also renamed some stuff to make more sense in context

--- makesite.go
+++ makesite.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"os"
+	"strings"
 	"text/template"
 )
 
-type HTMLtemplate struct {
+type HTMLcontents struct {
 	Content string
 }
 
 func main() {
-	fileContents, _ := ioutil.ReadFile("./first-post.txt")
+	fileInput := flag.String("file", "first-post.txt", "txt file to pass in")
+	flag.Parse()
 
-	templateStruct := HTMLtemplate{Content: string(fileContents)}
+	newHTML := strings.Split(*fileInput, ".")[0] + ".html"
+
+	fileContents, _ := ioutil.ReadFile(*fileInput)
+
+	templateStruct := HTMLcontents{Content: string(fileContents)}
 
 	parsedTemplate, _ := template.ParseFiles("template.tmpl")
 
-	newFile, _ := os.Create("first-post.html")
+	newFile, _ := os.Create(newHTML)
 
 	err := parsedTemplate.Execute(newFile, templateStruct)
-
 	if err != nil {
 		panic(err)
 	}
